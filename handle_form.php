1250746fdb5c8e0ec9ac8183b6d45369144c6deb
Alex Tan <asjtan@gmail.com@users.noreply.github.com>
2024-06-19 19:46:18 +0800

    fix json

--- handle_form.php
+++ handle_form.php
@@ -148,7 +148,7 @@ switch($exampleRadios) {
                 "official_email_address" => $email_address,
                 "project_id_if_applicable" => $projec_id,
                 'Stakeholder_collaborators'=> 'true',
-                "research_collaboration" => "Sponsor's Collaboration",
+                "research_collaboration" => "Stakeholder_collaborators",
                 "first_nameStakeholder" => $sponsorfirstname,
                 "last_nameStakeholder" => $sponsorlastname,
                 "nationalityofStakehoders" => $sponsornationality,
